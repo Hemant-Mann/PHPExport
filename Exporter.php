06ac29d1a396cea00afc3250f857afed447b9fa3
Hemant <hemant.mann121@gmail.com>
2015-07-06 22:05:53 +0530

    Updated Autoloader

--- Exporter.php
+++ Exporter.php
@@ -2,7 +2,7 @@
 define("LIB_PATH", dirname(dirname(__FILE__)));
 
 spl_autoload_register(function($class) {
-    $path = lcfirst(str_replace("\\", DIRECTORY_SEPARATOR, $class));
+    $path = str_replace("\\", DIRECTORY_SEPARATOR, $class);
     $file = LIB_PATH."/{$path}.php";
     
     if (file_exists($file)) {
